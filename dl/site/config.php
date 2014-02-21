10f63b05e5bee9b607084301383047bda0eef1eb
Mike <michael.billington@gmail.com>
2014-02-21 15:46:17 +1100

    add new files from update

--- dl/site/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-/* Database connection options */
-$config['database']['user'] = "devicelog";
-$config['database']['pass'] = "ChBB2CSD4b9KQrau";
-$config['database']['host'] = "localhost";
-$config['database']['db'] = "devicelog";
