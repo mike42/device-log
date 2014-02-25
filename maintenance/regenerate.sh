f79e3e0c691a6d1ebb80520aaf29a97f6cbba960
Mike <michael.billington@gmail.com>
2014-02-26 10:50:44 +1100

    add new generated code

---
+++ maintenance/regenerate.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Save the config
+echo "Saving"..
+mv ../dl/site ../
+rm -Rf ../dl
+
+# Regenerate
+echo "Generating.."
+cd ../../php-mysql-model-generator
+rm -Rf dl && ./generate.php dl < ../device-log/maintenance/database/device-log.sql
+mv dl ../device-log/
+cd ../device-log
+
+# Replace config
+echo "Replacing.."
+mv dl/site dl/site.default
+mv site dl/
