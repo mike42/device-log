f5a41a1218d0a5feb90d1f0735f46a5cef7330b7
Mike <michael.billington@gmail.com>
2014-03-06 11:35:48 +1100

    load device list

--- dl/public/js/dl.js
+++ dl/public/js/dl.js
@@ -1,28 +1,82 @@
+var DeviceCollection = Backbone.Collection.extend({
+	url : '/dl/api/device/list_all/1/100',
+	model : device_model
+});
+
+var DeviceView = Backbone.View.extend({
+	template : _.template($('#device-template').html()),
+	tagName : 'tr',
+	// el: 'div#device-table',
+
+	initialize : function(options) {
+		_.bindAll(this, 'render');
+		this.model.bind('change', this.render);
+	},
+
+	render : function() {
+		this.$el.html(this.template(this.model.toJSON()));
+		return this;
+	}
+});
+
+var DevicesView = Backbone.View.extend({
+	collection : null,
+	el : 'tbody#device-tbody',
+
+	initialize : function(options) {
+		this.collection = options.collection;
+		this.collection.bind('reset', this.render);
+		this.collection.bind('add', this.render);
+		this.collection.bind('remove', this.render);
+	},
+
+	render : function() {
+		var element = this.$el;
+		element.empty();
+
+		this.collection.forEach(function(item) {
+			var itemView = new DeviceView({
+				model : item
+			});
+			element.append(itemView.template(itemView.model.toJSON()));
+		});
+		return this;
+	}
+});
+
 function foobar() {
-	var a = new device_model({id: 23});
-	a.fetch({
-		success: function(results) {
+	var devices = new DeviceCollection();
+	devices.fetch({
+		success : function(results) {
+			var db = new DevicesView({
+				collection : devices
+			});
+			db.render();
 			console.log(results);
 		}
 	});
-	var a = new device_model({sn: 'aaa', person_id: 1, device_status_id: 1, device_type_id: 1});
-	
-	var ImageView = Backbone.View.extend({
-	    el: 'div#img',
-	    initialize: function () {
-	        this.render();
-	    },
-	    render: function () {
-	        this.$el.html( this.model.get('title') ) ;
-	        return this;
-	    }
-	});
+
+	// var a = new device_model({
+	// id : 23
+	// });
+	// a.fetch({
+	// success : function(results) {
+	// var firstView = new DeviceView({
+	// model : a
+	// });
+	// console.log(results);
+	// }
+	// });
+	// var a = new device_model({sn: 'aaa', person_id: 1, device_status_id: 1,
+	// device_type_id: 1});
+
 	// create new view
-	var firstView = new ImageView({model: firstImage});
+	// var firstView = new DeviceView({
+	// model : a
+	// });
 
+	// a.save();
 
-	Read more: http://mrbool.com/backbone-js-view/27972#ixzz2v3GslpLY
-	//a.save();
 }
 
 $('#btnLogout').on('click', function(event) {
